a4d301cfe9966556d71ec871236c5e1e54e4a623
Jang Heeji <99714448+whitez1@users.noreply.github.com>
2023-02-06 22:46:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,22 +1,26 @@
 def solution(id_list, report, k):
-    answer = [0]*len(id_list)
-    lst = [[] for _ in range(len(id_list))]
-    report_lst = [0]*len(id_list)
-    stop_id = []    
-    for i in report:    # 신고 당한 사람 list 작성
-        report_reported = i.split()
-        r = id_list.index(report_reported[0])
-        if report_reported[1] not in lst[r]:
-            lst[r].append(report_reported[1])
-            report_lst[id_list.index(report_reported[1])] += 1
-            
-    for idx, j in enumerate(report_lst): # 정지된 사람 list
-        if j >= k:
-            stop_id.append(id_list[idx])
+    answer = []
+    matrix = [[0]*len(id_list) for _ in range(len(id_list))]
+    for i in report:
+        a, b = i.split()
+        r=id_list.index(a)
+        c=id_list.index(b)
+        matrix[r][c]=1
+    report_count=[]  
+    for r in range(len(id_list)):
+        sum=0
+        for c in range(len(id_list)):
+            sum+=matrix[c][r]
+        report_count.append(sum)
+
+    stop_id_idx=[]
+    for j in range(len(id_list)):
+        if report_count[j] >= k:
+            stop_id_idx.append(j)
             
     for r in range(len(id_list)):
-        for c in range(len(lst[r])):
-            if lst[r][c] in stop_id:
-                answer[r] += 1
-    
+        sum=0
+        for c in stop_id_idx:
+            sum+=matrix[r][c]
+        answer.append(sum)        
     return answer
