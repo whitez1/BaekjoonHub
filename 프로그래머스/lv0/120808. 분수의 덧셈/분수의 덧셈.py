e68733be22722d45458ea7501ffc0dda541424c3
Jang Heeji <99714448+whitez1@users.noreply.github.com>
2023-02-09 23:42:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.54 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -2,15 +2,11 @@ def solution(numer1, denom1, numer2, denom2):
     answer = []
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
-    if numer < denom:
-        for n in range(numer, 1, -1):
-            if denom % n ==0 and numer % n == 0:
-                denom /= n
-                numer /= n
-    else:
-        for d in range(denom, 1, -1):
-            if denom % d ==0 and numer % d == 0:
-                denom /= d
-                numer /= d
+   
+    for n in range(min(numer, denom), 1, -1):
+        if denom % n ==0 and numer % n == 0:
+            denom /= n
+            numer /= n
+   
     answer = [numer, denom]
     return answer
