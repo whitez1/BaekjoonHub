ee80d683b15e8fdd329d6070654bf7e01b988d55
Jang Heeji <99714448+whitez1@users.noreply.github.com>
2023-02-09 23:03:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,14 +1,10 @@
 def solution(babbling):
     answer = 0
     possible = ['aya', 'ye', 'woo', 'ma']
-    cnt = [0]*4
-    for idx, bab in enumerate(babbling):
-        count = 0 
+
+    for bab in babbling:
         for pos in possible: 
-            if pos in bab:
-                count += 1
-                babbling[idx] = babbling[idx].replace(pos, '1')
-        if babbling[idx] == '1'*count:
+            bab = bab.replace(pos, ' ', 1)
+        if len(bab.strip()) == 0:
             answer += 1
-            
     return answer
