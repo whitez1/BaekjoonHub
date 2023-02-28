3b75f8b3cc65522108f3368f9ed3644fe7362171
Jang Heeji <99714448+whitez1@users.noreply.github.com>
2023-02-28 22:52:08 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,13 +1,2 @@
 def solution(sides):
-    answer = 0
-    ma = max(sides)
-    mi = min(sides)
-    for i in range(1, ma+1):
-        if (mi+i) > ma:
-            answer += 1
-            
-    j = ma + 1
-    while(sum(sides)>j):
-        answer += 1
-        j += 1
-    return answer
+   return min(sides) + sum(sides) - max(sides) - 1
