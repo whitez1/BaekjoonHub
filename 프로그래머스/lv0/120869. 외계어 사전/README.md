20d6c4747cae7d89bfc1eb885f27c2574f19e5fb
Jang Heeji <99714448+whitez1@users.noreply.github.com>
2023-03-01 00:57:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/README.md
+++ 프로그래머스/lv0/120869. 외계어 사전/README.md
@@ -1,10 +1,10 @@
 # [level 0] 외계어 사전 - 120869 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -1,11 +1,6 @@
-def solution(spell, dic):    
-    for d in dic:
-        a = True
-        for s in spell:
-            if s not in d:
-                a = False
-                break
-        if a == True:
-            return 1   
+def solution(spell, dic):
+    spell = set(spell)
+    for s in dic:
+        if not spell-set(s):
+            return 1
     return 2
-                
