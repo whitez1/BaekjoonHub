d884b228036c753312a758d7f3480916b41ddfb9
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2023-08-10 19:09:43 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,16 +1,17 @@
-def compress(s, length):
-    words = [s[i:i+length] for i in range(0, len(s), length)]
-    res = []
-    cur_word = words[0]
-    cur_cnt = 1
-    for pattern, compare in zip(words, words[1:] + ['']):
-        if pattern == compare: cur_cnt += 1
-        else:
-            res.append([cur_word, cur_cnt])
-            cur_word = compare
-            cur_cnt = 1
-    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
-
 def solution(s):
-    if len(s) == 1: return 1
-    else: return min(compress(s, length) for length in list(range(1, len(s) // 2 + 1)))
+    answer = len(s)
+    for x in range(1, len(s) // 2 + 1):
+        comp_len = 0
+        comp = ''
+        cnt = 1
+        for i in range(0, len(s) + 1, x):
+            temp = s[i:i+x]
+            if comp == temp: cnt += 1
+            elif comp != temp:
+                comp_len += len(temp)
+                if cnt > 1: comp_len += len(str(cnt))
+                cnt = 1
+                comp = temp
+        answer = min(answer, comp_len)
+        
+    return answer
