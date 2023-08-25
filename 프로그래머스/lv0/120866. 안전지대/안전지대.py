45780678f82010df9770adbd4b4398e76df25e66
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2023-08-25 16:52:50 +0900

    [level 0] Title: 안전지대, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,7 +1,6 @@
 cnt = 0
 def solution(board):
     global cnt
-    #좌상, 상, 우상, 좌, 우, 좌하, 하, 우하
     row = [-1, -1, -1, 0, 0, 1, 1, 1]
     col = [-1, 0, 1, -1, 1, -1, 0, 1]
     n = len(board)
@@ -9,8 +8,6 @@ def solution(board):
         for j in range(n):
             if board[i][j] == 1:
                 dfs(i, j, row, col, board)
-    print(cnt)
-    print(board)
     return n*n - cnt
 
 def dfs(r, c, row, col, board):
@@ -20,7 +17,7 @@ def dfs(r, c, row, col, board):
     for i in range(8):
         rr = r+row[i]
         cc = c+col[i]
-        if 0<=rr<len(board) and 0<= cc < len(board):
+        if 0 <= rr < len(board) and 0 <= cc < len(board):
             if board[rr][cc] == 0:
                 cnt += 1
                 board[rr][cc] = 2
