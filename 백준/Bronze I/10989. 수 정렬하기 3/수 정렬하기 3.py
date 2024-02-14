c58785edade7b988738edf72c781aba51c451edd
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-02-14 20:39:27 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9060 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze I/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+lst = [0]*10001
+for _ in range(n):
+    lst[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if lst[i] != 0:
+        for _ in range(lst[i]):
+            print(i)
+            
