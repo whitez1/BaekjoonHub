fe6fc1f91a5ce4f71364c65a930252cc644c1dca
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-03-28 14:57:51 +0900

    [Silver IV] Title: 큐 2, Time: 1524 ms, Memory: 175128 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n = int(input())
+queue = deque()
+for _ in range(n):
+    s = input().rstrip().split()
+    if s[0] == 'push':
+        queue.append(s[1])
+    elif s[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif s[0] == 'size':
+        print(len(queue))
+    elif s[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif s[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif s[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+
