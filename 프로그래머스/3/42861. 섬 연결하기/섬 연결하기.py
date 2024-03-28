5bd8aac44a88e2e8ca7658f7c7e7f68a85f6efeb
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-03-28 21:15:49 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,24 +1,15 @@
-def find(parent, x):
-    if parent[x] == x: return x
-    parent[x] = find(parent, parent[x])
-    return parent[x]
-
-def union(parent, x, y):
-    x = find(parent, x)
-    y = find(parent, y)
-    if x == y: return
-    parent[x] = y
-    
 def solution(n, costs):
-    answer = cnt = 0
-    parent = [i for i in range(n)]
-    costs = sorted(costs, key = lambda x: x[2])
-    
-    for i in range(len(costs)):
-        if find(parent, costs[i][0]) != find(parent, costs[i][1]):
-            union(parent, costs[i][0], costs[i][1])
-            answer += costs[i][2]
-            cnt += 1
-        if cnt == n: break
+    answer = 0
+    mat = [[0]*n for _ in range(n)]
+    for x, y, c in costs:
+        mat[x][y] = c
+        mat[y][x] = c
+    pre = 10000000
+    for i in range(n):
+        s = 0
+        for  j in range(n):
+            s += mat[i][j]
+        if pre > s:
+            pre = s
         
     return answer
