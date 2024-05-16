f9e3f6e7166c8983ccef0d85bfbe423bc7112c50
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-05-16 23:29:35 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -0,0 +1,32 @@
+from collections import deque
+def solution(board):
+    N = len(board)
+    paths = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    
+    def bfs(x, y, cost, path):
+        visited = [[0]*N for _ in range(N)]
+        for a in range(N):
+            for b in range(N):
+                if board[a][b] == 1: visited[a][b] = -1
+    
+        queue = deque()
+        queue.append((x, y, cost, path))
+    
+        while queue:
+            x, y, cost, path = queue.popleft()
+
+            for i in range(len(paths)):
+                nx = x + paths[i][0]
+                ny = y + paths[i][1]
+                
+                if 0<=nx<N and 0<=ny<N and visited[nx][ny]!=-1:
+                    if path == i: newcost = cost + 100
+                    else: newcost = cost + 600
+                        
+                    if visited[nx][ny]==0 or (visited[nx][ny]!=0 and visited[nx][ny]>newcost):    
+                        queue.append((nx, ny, newcost, i))
+                        visited[nx][ny] = newcost
+    
+        return visited[N-1][N-1]
+    
+    return min(bfs(0, 0, 0, 2), bfs(0, 0, 0, 3))
