cc11adf4adf455af295244e2be408d6c28842f15
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-10-17 19:51:02 +0900

    [level 3] Title: 이중우선순위큐, Time: 335.36 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,27 +1,24 @@
 import heapq
 def solution(operations):
-    answer = []
     max_heap = []
     min_heap = []
     heapq.heapify(max_heap)
     heapq.heapify(min_heap)
     
-    for op in operations: 
-        if op[0] == 'I':
-            c, num = op.split()
-            num = int(num)
-            heapq.heappush(max_heap, -1*num)
-            heapq.heappush(min_heap, num)
-        elif op == "D 1":
+    for op in operations:           
+        if op == "D 1":
             if not max_heap: continue
             x = heapq.heappop(max_heap)
-            min_heap.pop(min_heap.index(-1*x))
-            
-        else:
+            min_heap.pop(min_heap.index(-x))     
+        elif op == "D -1":
             if not min_heap: continue
             x = heapq.heappop(min_heap)
-            max_heap.pop(max_heap.index(-1*x))
+            max_heap.pop(max_heap.index(-x)) 
+        else:
+            num = int(op[2:])
+            heapq.heappush(max_heap, -num)
+            heapq.heappush(min_heap, num)
             
     if max_heap and min_heap:
-        return [-1*heapq.heappop(max_heap), heapq.heappop(min_heap)]
+        return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
     else: return [0, 0]
