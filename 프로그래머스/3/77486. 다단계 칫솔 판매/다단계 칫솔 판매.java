70c1748bdf7e994a3c25cc70c19e16796d26c049
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2024-10-30 22:25:34 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 27.09 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 class Node {
-    public String name;
-    public int proceeds;
-    public Node parent;
+    String name;
+    int proceeds;
+    Node parent;
     
     public Node(String name, int proceeds, Node parent) {
         this.name = name;
@@ -14,25 +14,25 @@ class Node {
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = new int[enroll.length];
+        
         HashMap<String, Node> map = new HashMap<>();
         
         Node root = new Node("-", 0, null);
         map.put("-", root);
         
-        for (int i=0; i<amount.length; i++) amount[i] *= 100;
-        
         for (int i = 0; i<enroll.length; i++) {
-            Node node = new Node(enroll[i], 0, null);
+            Node node = new Node(enroll[i], 0, map.get(referral[i]));
             map.put(enroll[i], node);
         }
         
-        for (int i = 0; i<enroll.length; i++) {
-            Node node = map.get(enroll[i]);
-            node.parent = map.get(referral[i]);
-        }
+        // for (int i = 0; i<enroll.length; i++) { 
+        //     Node node = map.get(enroll[i]);
+        //     node.parent = map.get(referral[i]);
+        // }
         
         for (int i = 0; i<seller.length; i++) {
+            
+            amount[i] *= 100;
             Node node = map.get(seller[i]);
             
             while (node != root) {
@@ -45,6 +45,7 @@ class Solution {
             }
         }
         
+        int[] answer = new int[enroll.length];
         for (int i=0; i<enroll.length; i++) {
             answer[i] = map.get(enroll[i]).proceeds;
         }
