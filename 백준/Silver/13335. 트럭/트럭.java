b21fa9d07083f6171724cf2e202c80d77c0ddfa8
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2025-01-08 00:38:14 +0900

    [Silver I] Title: 트럭, Time: 116 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,40 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int w = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		Queue<Integer> truck = new LinkedList<Integer>();
-		st = new StringTokenizer(br.readLine());
-		for(int i= 0; i< n; i++) {
-			truck.offer(Integer.parseInt(st.nextToken()));
-		}
-		int time = 0; 
-		int bw=0;
-		Queue<Integer> q = new LinkedList<Integer>();
-		for(int i =0; i<w ; i++) {
-			q.add(0);
-		}
-		while(!q.isEmpty()) {
-			time++;
-			bw-=q.poll();
-			if(!truck.isEmpty()) {
-				if(truck.peek()+bw<=L) {
-					bw+=truck.peek();
-					q.offer(truck.poll());
-				}else {
-					q.offer(0);
-				}
-			}
-		}
-		System.out.println(time);
-	}
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        ArrayDeque<Integer> trucks = new ArrayDeque<>();
+        for(int i=0; i<n; i++) {
+            trucks.addLast(Integer.parseInt(st.nextToken()));
+        }
+
+        ArrayDeque<Integer> bridge = new ArrayDeque<>();
+        for(int i=0; i<w; i++) {
+            bridge.addLast(0);
+        }
+
+        int time = 0;
+        int sum_weight = 0;
+
+        while(!bridge.isEmpty()) {
+            time++;
+            sum_weight -= bridge.pollFirst();
+            if(!trucks.isEmpty()){
+                if (trucks.peek() + sum_weight <= L) {
+                    sum_weight += trucks.peek();
+                    bridge.addLast(trucks.pollFirst());
+                   
+                } else {
+                    bridge.addLast(0);
+                }
+            }
+        }
+
+        bw.write(time+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
