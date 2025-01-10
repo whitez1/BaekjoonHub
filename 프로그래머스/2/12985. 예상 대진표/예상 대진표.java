04c708c7f47171e2469e760e9a2f10a5427407b5
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2025-01-10 18:46:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,28 +3,12 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {     
-        int answer = 0;
-        int k = (int) (Math.log((double)n) / Math.log(2.0));
-        int plus = (int) (Math.pow(2, k+1) - Math.pow(2, k) - 1.0);
-        System.out.println(plus);
-        a += plus;
-        b += plus;
-        while(true) {
-            if(a==b) {break;}
-            answer++;
-            if(a%2==0) {
-                a = (int) a/2;
-            } else {
-                a = (int) (a-1)/2;
-            }
-            
-            if(b%2==0) {
-                b = (int) b/2;
-            } else {
-                b = (int) (b-1)/2;
-            }   
+        int answer;
+        
+        for(answer = 0; a != b; answer++) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
         }
-
         return answer;
     }
 }
