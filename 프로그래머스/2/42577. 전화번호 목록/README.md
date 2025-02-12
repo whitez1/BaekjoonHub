636989cb0877ee646b2a15227617c3e7e0a74114
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2025-02-12 15:24:29 +0900

    [level 2] Title: 전화번호 목록, Time: 271.52 ms, Memory: 248 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 266 MB, 시간: 284.66 ms
+메모리: 248 MB, 시간: 271.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 12일 15:07:47
+2025년 02월 12일 15:24:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        HashSet<String> set = new HashSet<>();
-        for(String number : phone_book) {
-            set.add(number);
-        }
-        
+        HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
+      
         for(String number : phone_book) {
             for(int i=1; i<number.length(); i++) {
                 if(set.contains(number.substring(0, i))) {
