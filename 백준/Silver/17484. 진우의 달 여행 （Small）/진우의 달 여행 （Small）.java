93a0b0fca896dc11972ea3c60889f9aa869ddd20
Heeji Jang <99714448+whitez1@users.noreply.github.com>
2026-03-09 23:05:03 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 132 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int answer = Integer.MAX_VALUE;
+
+    public static void dfs(int[][] arr, int n, int m, int i, int j, int diff, int sum) {
+        int dy[] = {-1, 0, 1};
+
+        if(i==n) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+         
+        for(int k=0; k<dy.length; k++) {
+            int nextj = j + dy[k];
+            if (nextj < 0 || nextj >= m) {
+                continue;
+            }
+            if(j - nextj == diff) {
+                continue;
+            }
+            dfs(arr, n, m, i+1, nextj, j-nextj, sum+arr[i][nextj]);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for(int j=0; j<M; j++) {
+            dfs(arr, N, M, 1, j, -2, arr[0][j]);
+        }
+        
+        bw.write(answer+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
